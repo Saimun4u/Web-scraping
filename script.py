60a704b4e79dfb2fc1bad0dd19e0141d9c041789
Saimun <saimun4u@gmail.com>
2021-04-09 23:15:10 -0500

    #Importing module for web scraping #Specifying the address to be shown in google maps #Implementing logic

--- script.py
+++ script.py
@@ -1,2 +1,17 @@
-import webbrowser
-webbrowser.open('http://www.barcoderestaurantgroup.com/contact1.php')
+import webbrowser, sys, pyperclip
+
+# Address to be copied
+# https://www.google.ca/maps/place/739+Gerrard+St+E,+Toronto
+
+
+sys.argv  # ['script.py', '739', 'Gerrard', 'St', 'E', 'Toronto']
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+# https://www.google.ca/maps/place/<ADDRESS>
+
+webbrowser.open('https://www.google.ca/maps/place/' + address)
+
