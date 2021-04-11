39a91cfc042e211e26920164e869246493930161
Saimun <saimun4u@gmail.com>
2021-04-10 21:12:07 -0500

    #Importing module for web scraping #Specifying the address to be shown in google maps #Implementing logic #Running the code #Using the request module to download a file

--- script.py
+++ script.py
@@ -6,12 +6,27 @@ import webbrowser, sys, pyperclip
 
 sys.argv  # ['script.py', '739', 'Gerrard', 'St', 'E', 'Toronto']
 
-if len(sys.argv) > 1:
-    address = ' '.join(sys.argv[1:])
-else:
-    address = pyperclip.paste()
+# if len(sys.argv) > 1:
+#     address = ' '.join(sys.argv[1:])
+# else:
+#     address = pyperclip.paste()
+#
+# # https://www.google.ca/maps/place/<ADDRESS>
+#
+# webbrowser.open('https://www.google.ca/maps/place/' + address)
 
-# https://www.google.ca/maps/place/<ADDRESS>
 
-webbrowser.open('https://www.google.ca/maps/place/' + address)
+
+#Downloading from the web with the request module
+
+
+import requests
+
+res = requests.get('https://www.gutenberg.org/files/2148/2148-0.txt')
+# print(res.status_code)
+
+print(len(res.text))
+
+print((res.text[:500]))
+
 
