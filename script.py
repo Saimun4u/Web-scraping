7f2b379a4ba9182c44183aa11fc8811af7351a25
Saimun <saimun4u@gmail.com>
2021-04-11 22:56:00 -0500

    #Importing module for web scraping #Specifying the address to be shown in google maps #Implementing logic #Running the code #Using the request module to download a file #Using Beautifulsoup4 for web parsing

--- script.py
+++ script.py
@@ -20,13 +20,18 @@ sys.argv  # ['script.py', '739', 'Gerrard', 'St', 'E', 'Toronto']
 #Downloading from the web with the request module
 
 
-import requests
+# import requests
 
-res = requests.get('https://www.gutenberg.org/files/2148/2148-0.txt')
+# res = requests.get('https://www.gutenberg.org/files/2148/2148-0.txt')
 # print(res.status_code)
+#
+# print(len(res.text))
+#
+# print((res.text[:500]))
 
-print(len(res.text))
+import bs4, requests
 
-print((res.text[:500]))
+res = requests.get('https://www.daraz.com.bd/products/amazfit-gtr-2-amoled-curved-display-classic-stainless-steel-global-version-silver-i163490760-s1096062719.html?spm=a2a0e.searchlist.list.1.26273b1a3CPUoC&search=1')
 
+res.raise_for_status()
 
