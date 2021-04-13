621dde5e21ac9706b2a00a3cc21b12f4b30d332f
Saimun <saimun4u@gmail.com>
2021-04-12 23:26:04 -0500

    #Importing module for web scraping #Specifying the address to be shown in google maps #Implementing logic #Running the code #Using the request module to download a file #Using Beautifulsoup4 for web parsing #Controlling the Browser with the Selenium Module

--- script.py
+++ script.py
@@ -29,9 +29,34 @@ sys.argv  # ['script.py', '739', 'Gerrard', 'St', 'E', 'Toronto']
 #
 # print((res.text[:500]))
 
-import bs4, requests
+# import bs4, requests
+#
+# res = requests.get('https://www.aliexpress.com/item/1005002277130722.html?spm=a2g0o.detail.1000060.3.47096cd4CdTsuc&gps-id=pcDetailBottomMoreThisSeller&scm=1007.13339.169870.0&scm_id=1007.13339.169870.0&scm-url=1007.13339.169870.0&pvid=25843377-7383-442e-8885-9b0f00f39f01&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.13339.169870.0,pvid:25843377-7383-442e-8885-9b0f00f39f01,tpp_buckets:668%230%23131923%2324_668%230%23131923%2324_668%23888%233325%2320_668%23888%233325%2320_668%232846%238107%231934_668%232717%237565%23769_668%231000022185%231000066058%230_668%233468%2315609%23281_668%232846%238107%231934_668%232717%237565%23769_668%233164%239976%23550_668%233468%2315609%23281')
+#
+# res.raise_for_status()
+#
+# soup = bs4.BeautifulSoup(res.text, 'html.parser')
+#
+# elems = soup.select('#root > div > div.product-main > div > div.product-info > div.product-price > div.product-price-current > span')
+#
+# print(elems[0].text)
 
-res = requests.get('https://www.daraz.com.bd/products/amazfit-gtr-2-amoled-curved-display-classic-stainless-steel-global-version-silver-i163490760-s1096062719.html?spm=a2a0e.searchlist.list.1.26273b1a3CPUoC&search=1')
 
-res.raise_for_status()
+# import bs4, requests
+#
+# def getDarazPrice(productUrl):
+#     res = requests.get(productUrl)
+#     res.raise_for_status()
+#
+#     soup = bs4.BeautifulSoup(res.text, 'html.parser')
+#
+#     elems = soup.select('#module_product_price_1 > div > div > span')
+#
+#     return elems[0].text.strip()
+#
+# price = getDarazPrice('https://www.daraz.com.bd/products/amazfit-gtr-2-amoled-curved-display-classic-stainless-steel-global-version-silver-i163490760-s1096062719.html?spm=a2a0e.searchlist.list.1.26273b1a3CPUoC&search=1')
+
+
+import selenium
+
 
